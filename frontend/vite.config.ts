b92b40239f581d7bb7ef9c092e0c08385938da2e
Nilesh Chakraborty <nileshchakraborty@gmail.com>
2025-12-18 15:55:16 -0500

    Added git actions

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/coding-practice-visualized/',
   plugins: [react()],
   server: {
     proxy: {
